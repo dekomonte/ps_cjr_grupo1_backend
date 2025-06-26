e2bcae3bc44f76f87440dc94016cd12e77f36d62
Andressa Monteiro <dekomonte@yahoo.com.br>
2025-06-26 10:57:27 -0300

    feat: recursos REST gerados com endpoints de avaliacao, comentario e professor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ProfessorModule } from './professor/professor.module';
+import { AvaliacaoModule } from './avaliacao/avaliacao.module';
+import { ComentarioModule } from './comentario/comentario.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, ProfessorModule, AvaliacaoModule, ComentarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
