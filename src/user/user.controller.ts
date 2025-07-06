25124441a208e9e3368392ae990e7f217145d54d
Gabrielly Freitas <gabriellybatistaf45@gmail.com>
2025-07-05 21:13:27 -0300

    integração do login, cadastro e perfilusuario funcionam

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,11 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -17,7 +21,20 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  @UseInterceptors(FileInterceptor('foto_perfil'))
+  async create(
+    @Body() body: any,
+    @UploadedFile() file?: Express.Multer.File,
+  ) {
+    const createUserDto: CreateUserDto = {
+      nome: body.nome?.toString(),
+      email: body.email?.toString(),
+      senha: body.senha?.toString(),
+      curso: body.curso?.toString(),
+      departamento: body.departamento?.toString(),
+      foto_perfil: file ? new Uint8Array(file.buffer) : undefined,
+    };
+
     return this.userService.create(createUserDto);
   }
 
